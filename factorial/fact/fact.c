51d9d1bf352ada8fa1a46ea6f7fc0469b875d47f
marianaec19 <mariana.echavarriac@upb.edu.co>
2024-01-31 10:07:03 -0500

    Copie el código para verificar si el número ingresado es mayor que 0

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -17,7 +17,13 @@ int main(int argc, char *argv[]) {
     
     /*Implement the function that calculates if the argument is a prime number*/
 
-    /*Print the result*/
+    /*Print the result*/ 
+    /*It must be verified that the entered number is greater than or equal to zero*/
+    if(number < 0)
+    {
+        perror("El núnmero debe ser mayor a cero");
+        exit(1);
+    }
     
     return 0;
 }
